be9291b103c3f549c05c2ba0961ff09b830b7dcd
Juan Herrera <david.juanherrera@gmail.com>
2016-03-29 14:53:09 -0500

    Convo form completed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,48 +1,10 @@
-$(function() {
-	// var line1 = ref.child("line1");
-	// line1.on("value",function(snapshot){
-	// 	var html="";
-	// 	html += "<div id='"+line1.key()+"'>";
-	// 	html += "<p>"+snapshot.val().text+"</p>";
-	// 	html += '<button id="upvote" type="button" class="btn btn-success">Up</button>';
-	// 	html += '<button id="upvote" type="button" class="btn btn-danger">Down</button>';
-	// 	html += "</div>";
-	// 	$("#mainContainer").append(html);
-	// });
-
+$(function(){
+	// Get a database reference to our posts
+	var ref = new Firebase("https://epicconvos.firebaseio.com/");
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+	console.log(snapshot.val());
+}, function (errorObject) {
+	console.log("The read failed: " + errorObject.code);
 });
-
-var messageIndex=2;
-
-function addRow(){
-	var html='<tr><td><div class="col-xs-10"><label class="radio-inline">';
-	html+= '<input type="radio" name="radio'+messageIndex+'" id="inlineRadio1" value="option1"> You </label>';
-	html+= '</div></td><td><div class="col-xs-10"><label class="radio-inline">';
-	html+= '<input type="radio" name="radio'+messageIndex+'" id="inlineRadio2" value="option2"> Not you</label>';
-	html+= '</div></td><td><div class="col-xs-12">'
-	html+= '<input type="text" class="form-control" id="message'+messageIndex+'" placeholder="Message">';
-	html+= '</div></td></tr>';
-	$("table").append(html);
-	messageIndex++;
-}
-
-function submitConvo(){
-	var context=$("#context").val();
-
-	var ref = new Firebase('https://epicconvos.firebaseio.com/');
-	var convosRef = ref.child("convos");
-	var convo = convosRef.push({
-		context: context,
-		upvotes: 0,
-		downvotes: 0
-	});
-	var convoRef = convosRef.child(convo.key()).child("messages");
-	for (var i = 1; i <= messageIndex; i++) {
-		var you=$("input[type='radio'][name='radio"+i+"']:checked").val()=="you";
-		var message = $("#message"+i).val();
-		convoRef.push({
-			you: you,
-			message: message
-		});
-	}
-}
+});
---
+++ javascripts/update.js
@@ -0,0 +1,49 @@
+$(function() {
+	$("#formConvo").submit(function( event ) {
+		event.preventDefault();
+		submitConvo();
+	});
+});
+
+var messageIndex=1;
+
+function removeRow(row){
+	$("#tr"+row).remove();
+}
+
+function addRow(){
+	messageIndex++;
+	var html='<tr id="tr'+messageIndex+'"><td><div class="col-xs-10"><label class="radio-inline">';
+	html+= '<input type="radio" name="radio'+messageIndex+'" id="inlineRadio1" value="option1" required> You </label>';
+	html+= '</div></td><td><div class="col-xs-10"><label class="radio-inline">';
+	html+= '<input type="radio" name="radio'+messageIndex+'" id="inlineRadio2" value="option2"> Not you</label>';
+	html+= '</div></td><td><div class="col-xs-12">'
+	html+= '<input type="text" class="form-control" id="message'+messageIndex+'" placeholder="Message" required>';
+	html+= '</div></td><td><button type="button" onClick="removeRow('+messageIndex+')" class="btn btn-danger">X</button></td>';
+	html+= '</tr>';
+	$("table").append(html);
+}
+
+function submitConvo(){
+	var context=$("#context").val();
+
+	var ref = new Firebase('https://epicconvos.firebaseio.com/');
+	var convosRef = ref.child("convos");
+	var convo = convosRef.push({
+		context: context,
+		upvotes: 0,
+		downvotes: 0
+	});
+	var convoRef = convosRef.child(convo.key()).child("messages");
+	for (var i = 1; i <= messageIndex; i++) {
+		var message = $("#message"+i);
+		if (message.length){
+			var you=$("input[type='radio'][name='radio"+i+"']:checked").val()=="you";
+			convoRef.push({
+				you: you,
+				message: message.val()
+			});
+		}
+	}
+	$("#formConvo")[0].reset();
+}
